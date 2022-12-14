001233bbed635e48bb4a34cf6053107a57e08501
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2022-12-14 10:43:12 -0500

    updated match statement

--- day7/directory.py
+++ day7/directory.py
@@ -3,27 +3,23 @@ def build_directory_structure(parsed_input: list[list[str]]) -> dict[str, int]:
     cur_size: int = 0
 
     for line in parsed_input:
-        match line[0]:
-            case "ls":
-                continue
-            case "cd":
-                match line[1]:
-                    case "/":
-                        path = [line[1]]
-                        cur_path = line[1]
-                        directories.setdefault(cur_path, 0)
-                    case "..":
-                        cur_size = directories.get(cur_path)
-                        path.pop()
-                        cur_path = "".join(path)
-                        directories[cur_path] += cur_size
-                    case _:
-                        path.append(f"{line[1]}/")
-                        cur_path = "".join(path)
-            case "dir":
+        match line:
+            case [_, "cd", "/"]:
+                path = [line[2]]
+                cur_path = line[2]
+                directories.setdefault(cur_path, 0)
+            case [_, "cd", ".."]:
+                cur_size = directories.get(cur_path)
+                path.pop()
+                cur_path = "".join(path)
+                directories[cur_path] += cur_size
+            case [_, "cd", _]:
+                path.append(f"{line[2]}/")
+                cur_path = "".join(path)
+            case ["dir", _]:
                 directories.setdefault(f"{cur_path}{line[1]}/", 0)
-            case _:
-                directories[cur_path] += int(line[0])
+            case [num, _] if num.isdigit():
+                directories[cur_path] += int(num)
 
     for _ in range(len(path) - 1):
         cur_size = directories.get(cur_path)
@@ -35,10 +31,7 @@ def build_directory_structure(parsed_input: list[list[str]]) -> dict[str, int]:
 
 
 with open("input.txt", "r", encoding="utf-8") as file:
-    lines: list[str] = file.readlines()
-    parsed_input: list[list[str]] = [
-        line.strip().replace("$ ", "").split() for line in lines
-    ]
+    parsed_input: list[list[str]] = [line.strip().split() for line in file]
     dir_structure: dict[str, int] = build_directory_structure(parsed_input)
 
     print(sum([size for size in dir_structure.values() if size <= 100000]))
