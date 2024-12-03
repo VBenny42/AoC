4ffb7cf95a6eb9b7e3dcf8680be4254fb2ac9672
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2024-12-03 12:23:06 -0400

    refactor(day1): updated code to use readlines instead

--- 2024/day01/solution.py
+++ 2024/day01/solution.py
@@ -1,30 +1,31 @@
 def main1():
     diff_sum = 0
-    with open("input1-sorted.txt", "r") as i1:
-        with open("input2-sorted.txt", "r") as i2:
-            for _ in range(1000):
-                line1 = int(i1.readline().strip())
-                line2 = int(i2.readline().strip())
-                diff = abs(line1 - line2)
-                diff_sum += diff
+
+    with open("input1-sorted.txt", "rb") as i1:
+        i1_lines = i1.readlines()
+        i1_lines = map(int, i1_lines)
+    with open("input2-sorted.txt", "rb") as i2:
+        i2_lines = i2.readlines()
+        i2_lines = map(int, i2_lines)
+
+    for line1, line2 in zip(i1_lines, i2_lines):
+        diff = abs(line1 - line2)
+        diff_sum += diff
     print(diff_sum)
 
 
 def main2():
     from collections import Counter
 
-    i1_counter = Counter()
-    with open("input1-sorted.txt", "r") as i1:
-        for line in i1:
-            # Read and process each line
-            line = int(line.strip())
-            i1_counter.update([line])
-    i2_counter = Counter()
-    with open("input2-sorted.txt", "r") as i2:
-        for line in i2:
-            # Read and process each line
-            line = int(line.strip())
-            i2_counter.update([line])
+    with open("input1-sorted.txt", "rb") as i1:
+        lines = i1.readlines()
+        lines = map(int, lines)
+        i1_counter = Counter(lines)
+    with open("input2-sorted.txt", "rb") as i2:
+        lines = i2.readlines()
+        lines = map(int, lines)
+        i2_counter = Counter(lines)
+
     similarity_sum = 0
     for key in i1_counter.keys():
         similarity_sum += (key * i1_counter[key]) * i2_counter.get(key, 0)
