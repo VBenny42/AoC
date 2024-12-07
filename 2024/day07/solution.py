a7c2e9d612b72ada0cc105fb9edd238cdd21db38
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2024-12-07 15:35:48 -0400

    feat(7): added backwards recursion

--- 2024/day07/solution.py
+++ 2024/day07/solution.py
@@ -1,80 +1,151 @@
 import operator
+from math import ceil, log10
+from time import time
 from typing import List, Tuple
 
 Equation = Tuple[int, List[int]]
 
 
-def is_valid_equation1(equation: Equation) -> bool:
-    operators = {operator.add, operator.mul}
-    test_value, numbers = equation
+def timer_func(func):
+    # This function shows the execution time of
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f"Function {func.__name__!r} executed in {(t2-t1):.4f}s")
+        return result
+
+    return wrap_func
+
+
+def is_valid_equation(equation: Equation, operators) -> bool:
+    desired_value, numbers = equation
     if len(numbers) == 2:
-        return any(test_value == op(*numbers) for op in operators)
+        return any(desired_value == op(*numbers) for op in operators)
     # More than 2 remaining_numbers
     # Solve for the first two numbers and recurse
     first, second, *remaining_numbers = numbers
     return any(
-        is_valid_equation1((test_value, [op(first, second)] + remaining_numbers))
+        is_valid_equation(
+            (desired_value, [op(first, second)] + remaining_numbers), operators
+        )
         for op in operators
     )
+
+
+def ends_with(a: int, b: int) -> int:
+    str_a, str_b = str(a), str(b)
+    if str_a.endswith(str_b):
+        remaining = str_a[: -len(str_b)] if len(str_a) > len(str_b) else "0"
+        return int(remaining)
+    return 0
+
+
+def is_valid_equation_p1(equation: Equation) -> bool:
+    desired_value, numbers = equation
+    if len(numbers) == 2:
+        return (
+            desired_value == numbers[0] + numbers[1]
+            or desired_value == numbers[0] * numbers[1]
+        )
+    last = numbers[-1]
+    mult, add = False, False
+    if desired_value % last == 0:
+        mult = is_valid_equation_p1((int(desired_value / last), numbers[:-1]))
+    if desired_value - last >= 0:
+        add = is_valid_equation_p1((desired_value - last, numbers[:-1]))
+    return any([mult, add])
+
+
+def is_valid_equation_p2(equation: Equation) -> bool:
+    desired_value, numbers = equation
+    if len(numbers) == 2:
+        return (
+            desired_value == numbers[0] + numbers[1]
+            or desired_value == numbers[0] * numbers[1]
+            or desired_value == concat_op(numbers[0], numbers[1])
+        )
+    last = numbers[-1]
+    mult, add, concat = False, False, False
+    if desired_value % last == 0:
+        mult = is_valid_equation_p2((int(desired_value / last), numbers[:-1]))
+    if desired_value - last >= 0:
+        add = is_valid_equation_p2((desired_value - last, numbers[:-1]))
+    if concat := ends_with(desired_value, last):
+        concat = is_valid_equation_p2((concat, numbers[:-1]))
+    return any([mult, add, concat])
 
 
 def concat_op(a, b):
-    return int(str(a) + str(b))
-
-
-def is_valid_equation2(equation: Equation) -> bool:
-    operators = {operator.add, operator.mul, concat_op}
-    test_value, numbers = equation
-    if len(numbers) == 2:
-        return any(test_value == op(*numbers) for op in operators)
-    # More than 2 remaining_numbers
-    # Solve for the first two numbers and recurse
-    first, second, *remaining_numbers = numbers
-    return any(
-        is_valid_equation2((test_value, [op(first, second)] + remaining_numbers))
-        for op in operators
-    )
+    return a * (10 ** ceil(log10(b + 1))) + b
 
 
 def print_equation(equation: Equation) -> None:
-    test_value, numbers = equation
-    print(test_value, end=": ")
+    desired_value, numbers = equation
+    print(desired_value, end=": ")
     print(" ".join(map(str, numbers)), end="")
     print()
 
 
-def main1():
+@timer_func
+def main1_forward():
     with open("input.txt", "r") as f:
         equations = [
-            (int(test_value), list(map(int, remaining_numbers.split())))
+            (int(desired_value), list(map(int, remaining_numbers.split())))
             for line in f
-            for test_value, remaining_numbers in [line.split(":")]
+            for desired_value, remaining_numbers in [line.split(":")]
         ]
 
     print(
-        f"LOGF: Sum of true equations: {sum(equation[0] for equation in equations if is_valid_equation1(equation))}"
+        f"LOGF: Sum of true equations: {sum(equation[0] for equation in equations if is_valid_equation(equation, {operator.add, operator.mul}))}"
     )
 
 
-def main2():
+@timer_func
+def main1_backward():
     with open("input.txt", "r") as f:
         equations = [
-            (int(test_value), list(map(int, remaining_numbers.split())))
+            (int(desired_value), list(map(int, remaining_numbers.split())))
             for line in f
-            for test_value, remaining_numbers in [line.split(":")]
+            for desired_value, remaining_numbers in [line.split(":")]
         ]
 
-    # for equation in equations:
-    #     if is_valid_equation1(equation):
-    #         print_equation(equation)
-    #     if is_valid_equation2(equation):
-    #         print_equation(equation)
+    print(
+        f"LOGF: Sum of true equations: {sum(equation[0] for equation in equations if is_valid_equation_p1(equation))}"
+    )
+
+
+@timer_func
+def main2_forward():
+    with open("input.txt", "r") as f:
+        equations = [
+            (int(desired_value), list(map(int, remaining_numbers.split())))
+            for line in f
+            for desired_value, remaining_numbers in [line.split(":")]
+        ]
 
     print(
-        f"LOGF: Sum of true equations: {sum(equation[0] for equation in equations if is_valid_equation2(equation))}"
+        f"LOGF: Sum of true equations: {sum(equation[0] for equation in equations if is_valid_equation(equation, {operator.add, operator.mul, concat_op}))}"
+    )
+
+
+@timer_func
+def main2_backward():
+    with open("input.txt", "r") as f:
+        equations = [
+            (int(desired_value), list(map(int, remaining_numbers.split())))
+            for line in f
+            for desired_value, remaining_numbers in [line.split(":")]
+        ]
+
+    print(
+        f"LOGF: Sum of true equations with concat: {sum(equation[0] for equation in equations if is_valid_equation_p2(equation))}"
     )
 
 
 if __name__ == "__main__":
-    # main1()
-    main2()
+    main1_forward()
+    main1_backward()
+    main2_forward()
+    main2_backward()
