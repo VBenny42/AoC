3570ecbcf5ec7226a615a6d7b870c8aa5005e2a2
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2024-12-11 17:30:30 -0400

    feat(day11): part 2 done

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+from math import floor, log10
 from typing import List
 
 
@@ -5,38 +7,52 @@ def apply_rules(stone: int) -> List[int]:
     if stone == 0:
         return [1]
 
-    length = 0
-    temp = stone
-
-    while temp > 0:
-        length += 1
-        temp //= 10
+    length = floor(log10(stone)) + 1
 
     if length % 2 == 0:
         split_point = length // 2
-        first_half = stone // 10 ** split_point
-        second_half = stone % 10 ** split_point
+        first_half = stone // 10**split_point
+        second_half = stone % 10**split_point
         return [first_half, second_half]
 
     return [stone * 2024]
 
+@cache
+def apply_rules_recursive(stone: int, iterations: int) -> int:
+    if iterations == 0:
+        return 1
+    if stone == 0:
+        return apply_rules_recursive(1, iterations - 1)
+    length = floor(log10(stone)) + 1
+    if length % 2 == 0:
+        split_point = length // 2
+        first_half = stone // 10**split_point
+        second_half = stone % 10**split_point
+        return apply_rules_recursive(first_half, iterations - 1) + apply_rules_recursive(second_half, iterations - 1)
+    return apply_rules_recursive(stone * 2024, iterations - 1)
 
 def blink(stones: List[int]) -> list[int]:
-    # new_stones = []
-    # for stone in stones:
-    #     new_stones.extend(apply_rules(stone))
-    # return new_stones
-    return [stone for stone in stones for stone in apply_rules(stone)]
+    return [new_stone for stone in stones for new_stone in apply_rules(stone)]
+
+def blink_recursive(stones: List[int], iterations: int) -> int:
+    return sum(apply_rules_recursive(stone, iterations) for stone in stones)
+
 
 def main1():
     with open("input.txt", "r") as f:
         stones = list(map(int, f.read().split()))
-    # print(f"LOG: { stones = }")
     for _ in range(25):
         stones = blink(stones)
-        # print(f"LOG: { stones = }")
-    print(f"LOG: { len(stones) = }")
+    print(f"LOGF: { len(stones) = }")
+
+
+def main2():
+    with open("input.txt", "r") as f:
+        stones = list(map(int, f.read().split()))
+    stones = blink_recursive(stones, 75)
+    print(f"LOG: { stones = }")
 
 
 if __name__ == "__main__":
     main1()
+    main2()
