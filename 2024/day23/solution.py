49ddfa36d08d01888d9d8b4af52f06fcda0d59f7
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2024-12-23 14:29:50 -0400

    feat(day23):  part 2 done

--- 2024/day23/solution.py
+++ 2024/day23/solution.py
@@ -16,18 +16,24 @@ def main1():
             break
 
     # Filter for cliques containing at least one node starting with 't'
-    triangles_with_t = [
-        triangle
-        for triangle in triangles
-        if any(node.startswith("t") for node in triangle)
-    ]
+    triangles_with_t = sum(
+        1 for triangle in triangles if any(node.startswith("t") for node in triangle)
+    )
 
     # Print results
-    print(f"ANSWER1: {len(triangles_with_t)}")
+    print(f"ANSWER1: {(triangles_with_t)}")
 
 
 def main2():
-    pass
+    G = nx.Graph()
+    with open("input.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            u, v = line.strip().split("-")
+            G.add_edge(u, v)
+
+    max_clique = max(nx.find_cliques(G), key=len)
+    max_clique = ",".join(sorted(max_clique))
+    print(f"ANSWER2: { max_clique }")
 
 
 if __name__ == "__main__":
