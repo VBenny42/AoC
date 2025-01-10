ff181419a80d090e917a41dae1f9ec56b0b4936c
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-01-10 19:12:20 -0400

    feat(2022): part 2 working

--- 2022/golang/day15/day15.go
+++ 2022/golang/day15/day15.go
@@ -2,6 +2,7 @@ package day15
 
 import (
 	"fmt"
+	"runtime"
 	"sort"
 
 	"github.com/VBenny42/AoC/2022/golang/utils"
@@ -15,6 +16,7 @@ type pair struct {
 type day15 struct {
 	pairs      []pair
 	rowToCheck int
+	bounds     int
 }
 
 type kleePoint struct {
@@ -32,11 +34,9 @@ func manhattanDistance(a, b utils.Coord) int {
 }
 
 func (p *pair) manhattanSpan(row int) (span, bool) {
-	radius := manhattanDistance(p.sensor, p.beacon)
-
 	dy := utils.Abs(p.sensor.Y - row)
 
-	if dx := radius - dy; dx >= 0 {
+	if dx := manhattanDistance(p.sensor, p.beacon) - dy; dx >= 0 {
 		return span{p.sensor.X - dx, p.sensor.X + dx}, true
 	} else {
 		return span{}, false
@@ -67,16 +67,17 @@ func buildKleePoints(row int, pairs []pair) []kleePoint {
 	return points
 }
 
+// https://iq.opengenus.org/klee-algorithm/
 func calculateTotalLength(points []kleePoint) int {
 	totalLength := 0
 	depth := 1
 
-	for i := 1; i < len(points); i++ {
-		prev, curr := points[i-1], points[i]
-		if diff := curr.x - prev.x; depth > 0 && diff > 0 {
+	for i := range len(points) - 1 {
+		curr, next := points[i], points[i+1]
+		if diff := next.x - curr.x; depth > 0 && diff > 0 {
 			totalLength += diff
 		}
-		if curr.isEnd {
+		if next.isEnd {
 			depth--
 		} else {
 			depth++
@@ -85,12 +86,109 @@ func calculateTotalLength(points []kleePoint) int {
 	return totalLength
 }
 
+func (d *day15) findVacantPoint() utils.Coord {
+	beacons := d.beaconsByRow()
+
+	results := make(chan utils.Coord)
+	failed := make(chan bool)
+
+	numWorkers := runtime.NumCPU()
+
+	for row := 0; row < numWorkers; row++ {
+		go checkRows(row, numWorkers, d.bounds, d.pairs, beacons, results, failed)
+	}
+
+	for remaining := numWorkers; remaining > 0; {
+		select {
+		case <-failed:
+			remaining--
+		case result := <-results:
+			// Guaranteed to have one vacant point, so return immediately
+			return result
+		}
+	}
+
+	return utils.Coord{}
+}
+
+func tuningFrequency(c utils.Coord) int {
+	return c.X*4000000 + c.Y
+}
+
+func checkRows(
+	startRow int,
+	step int,
+	bounds int,
+	pairs []pair,
+	beacons map[int][]span,
+	results chan utils.Coord,
+	failed chan bool,
+) {
+	spans := make([]span, 0, len(pairs))
+
+	for row := startRow; row < bounds; row += step {
+		spans := spans[:0]
+
+		if beaconsInRow, ok := beacons[row]; ok {
+			spans = append(spans, beaconsInRow...)
+		}
+
+		for _, p := range pairs {
+			if span, ok := p.manhattanSpan(row); ok {
+				spans = append(spans, span)
+			}
+		}
+
+		sort.Slice(spans, func(i, j int) bool {
+			return spans[i].start < spans[j].start
+		})
+
+		currentSpan := spans[0]
+
+		for _, s := range spans[1:] {
+			if s.start <= currentSpan.end+1 {
+				if s.end > currentSpan.end {
+					currentSpan.end = s.end
+				}
+			} else {
+				// Found a gap
+				results <- utils.Coord{X: currentSpan.end + 1, Y: row}
+				return
+			}
+		}
+	}
+
+	failed <- true
+}
+
+func (d *day15) beaconsByRow() map[int][]span {
+	beacons := make(map[int][]span)
+
+	for _, p := range d.pairs {
+		row := p.beacon.Y
+		beaconSpan := span{p.beacon.X, p.beacon.X}
+
+		if spans, ok := beacons[row]; ok {
+			beacons[row] = append(spans, beaconSpan)
+		} else {
+			beacons[row] = []span{beaconSpan}
+		}
+	}
+
+	return beacons
+}
+
 func (d *day15) Part1() int {
 	points := buildKleePoints(d.rowToCheck, d.pairs)
 	return calculateTotalLength(points)
 }
 
-func Parse(filename string, rowToCheck int) *day15 {
+func (d *day15) Part2() int {
+	vacantPoint := d.findVacantPoint()
+	return tuningFrequency(vacantPoint)
+}
+
+func Parse(filename string, rowToCheck, bounds int) *day15 {
 	data := utils.ReadLines(filename)
 
 	pairs := make([]pair, len(data))
@@ -100,11 +198,12 @@ func Parse(filename string, rowToCheck int) *day15 {
 			&pairs[i].sensor.X, &pairs[i].sensor.Y, &pairs[i].beacon.X, &pairs[i].beacon.Y)
 	}
 
-	return &day15{pairs, rowToCheck}
+	return &day15{pairs, rowToCheck, bounds}
 }
 
 func Solve(filename string) {
-	day := Parse(filename, 2000000)
+	day := Parse(filename, 2000000, 4000000)
 
 	fmt.Println("ANSWER1: number of positions with no beacon in row 2000000:", day.Part1())
+	fmt.Println("ANSWER2: tuning frequency of the only vacant point:", day.Part2())
 }
