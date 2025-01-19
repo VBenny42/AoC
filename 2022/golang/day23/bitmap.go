d7a1c4be7a6a20f4ae8769e9c28911436c96a20c
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-01-19 14:19:45 -0400

    feat(2022): switched to bitmap for day23, happy with optimization

---
+++ 2022/golang/day23/bitmap.go
@@ -0,0 +1,189 @@
+package day23
+
+import (
+	"strings"
+
+	"github.com/VBenny42/AoC/2022/golang/utils"
+)
+
+const offset = 20
+
+type bitmap [160][160]bool
+
+func (b *bitmap) rounds(limit int) int {
+	var (
+		rounds    int
+		direction int
+	)
+
+	neighbours := make([]bool, 12)
+
+	for {
+		rounds++
+
+		var (
+			proposed   = map[utils.Coord]utils.Coord{}
+			cannotMove = map[utils.Coord]struct{}{}
+		)
+
+		var elves, still int
+
+		for y, row := range *b {
+			for x := range row {
+				if !(*b)[y][x] {
+					continue
+				}
+				elves++
+				current := utils.Coord{X: x - offset, Y: y - offset}
+
+				// NW
+				neighbours[0] = b.get(current.Add(utils.Up).Add(utils.Left))
+				// N
+				neighbours[1] = b.get(current.Add(utils.Up))
+				// NE
+				neighbours[2] = b.get(current.Add(utils.Up).Add(utils.Right))
+
+				// SW
+				neighbours[3] = b.get(current.Add(utils.Down).Add(utils.Left))
+				// S
+				neighbours[4] = b.get(current.Add(utils.Down))
+				// SE
+				neighbours[5] = b.get(current.Add(utils.Down).Add(utils.Right))
+
+				// WS
+				neighbours[6] = neighbours[3]
+				// W
+				neighbours[7] = b.get(current.Add(utils.Left))
+				// WN
+				neighbours[8] = neighbours[0]
+
+				// ES
+				neighbours[9] = neighbours[5]
+				// E
+				neighbours[10] = b.get(current.Add(utils.Right))
+				// EN
+				neighbours[11] = neighbours[2]
+
+				allEmpty := true
+				for _, neighbour := range neighbours {
+					if neighbour {
+						allEmpty = false
+						break
+					}
+				}
+				if allEmpty {
+					still++
+					continue
+				}
+
+				for i := direction; i < direction+4; i++ {
+					dir := i % 4
+
+					allEmpty = true
+					for _, neighbour := range neighbours[dir*3 : dir*3+3] {
+						if neighbour {
+							allEmpty = false
+							break
+						}
+					}
+					if allEmpty {
+						next := current.Add(utils.Directions[dir])
+
+						_, ok := proposed[next]
+
+						if ok {
+							// Elf already proposed to move here
+							cannotMove[next] = struct{}{}
+						} else {
+							proposed[next] = current
+						}
+
+						break
+					}
+				}
+			}
+		}
+
+		if still == elves {
+			return rounds
+		}
+
+		direction = (direction + 1) % 4
+
+		for next, current := range proposed {
+			_, ok := cannotMove[next]
+			if ok {
+				continue
+			}
+
+			b.unset(current)
+			b.set(next)
+		}
+
+		if rounds == limit {
+			var (
+				bounds = b.bounds()
+				height = bounds.maxY - bounds.minY + 1
+				width  = bounds.maxX - bounds.minX + 1
+				size   = height * width
+			)
+
+			return size - elves
+		}
+
+	}
+}
+
+func (b *bitmap) set(c utils.Coord) {
+	(*b)[c.Y+offset][c.X+offset] = true
+}
+
+func (b *bitmap) get(c utils.Coord) bool {
+	return (*b)[c.Y+offset][c.X+offset]
+}
+
+func (b *bitmap) unset(c utils.Coord) {
+	(*b)[c.Y+offset][c.X+offset] = false
+}
+
+func (b *bitmap) bounds() bounds {
+	var minX, maxX, minY, maxY int
+	minX, minY = offset, offset
+
+	for y, row := range *b {
+		for x, cell := range row {
+			if cell {
+				if y < minY {
+					minY = y
+				} else if y > maxY {
+					maxY = y
+				}
+
+				if x < minX {
+					minX = x
+				} else if x > maxX {
+					maxX = x
+				}
+			}
+		}
+	}
+
+	return bounds{minX - offset, maxX - offset, minY - offset, maxY - offset}
+}
+
+func (b bitmap) String() string {
+	var builder strings.Builder
+
+	for y := range b {
+		for x := range b[y] {
+			if b[y][x] {
+				builder.WriteByte('#')
+			} else {
+				builder.WriteByte('.')
+			}
+		}
+		builder.WriteByte('\n')
+	}
+
+	return builder.String()
+}
--- 2022/golang/day23/grid.go
+++ 2022/golang/day23/grid.go
@@ -6,6 +6,130 @@ import (
 	"github.com/VBenny42/AoC/2022/golang/utils"
 )
 
+type grid map[int]map[int]struct{}
+
+func (g *grid) rounds(limit int) int {
+	var (
+		rounds  int
+		dirIter int
+	)
+
+	for {
+		rounds++
+
+		var (
+			proposed   = map[utils.Coord]utils.Coord{}
+			cannotMove = map[utils.Coord]struct{}{}
+		)
+
+		var elves, still int
+
+		for y, row := range *g {
+			for x := range row {
+				elves++
+				current := utils.Coord{X: x, Y: y}
+
+				neighbours := make([]bool, 12)
+
+				// NW
+				neighbours[0] = g.get(current.Add(utils.Up).Add(utils.Left))
+				// N
+				neighbours[1] = g.get(current.Add(utils.Up))
+				// NE
+				neighbours[2] = g.get(current.Add(utils.Up).Add(utils.Right))
+
+				// SW
+				neighbours[3] = g.get(current.Add(utils.Down).Add(utils.Left))
+				// S
+				neighbours[4] = g.get(current.Add(utils.Down))
+				// SE
+				neighbours[5] = g.get(current.Add(utils.Down).Add(utils.Right))
+
+				// WS
+				neighbours[6] = neighbours[3]
+				// W
+				neighbours[7] = g.get(current.Add(utils.Left))
+				// WN
+				neighbours[8] = neighbours[0]
+
+				// ES
+				neighbours[9] = neighbours[5]
+				// E
+				neighbours[10] = g.get(current.Add(utils.Right))
+				// EN
+				neighbours[11] = neighbours[2]
+
+				allEmpty := true
+				for _, neighbour := range neighbours {
+					if neighbour {
+						allEmpty = false
+						break
+					}
+				}
+				if allEmpty {
+					still++
+					continue
+				}
+
+				d := dirIter % 4
+				for i := d; i < d+4; i++ {
+					dir := i % 4
+
+					allEmpty = true
+					for _, neighbour := range neighbours[dir*3 : dir*3+3] {
+						if neighbour {
+							allEmpty = false
+							break
+						}
+					}
+					if allEmpty {
+						next := current.Add(utils.Directions[dir])
+
+						_, ok := proposed[next]
+
+						if ok {
+							// Elf already proposed to move here
+							cannotMove[next] = struct{}{}
+						} else {
+							proposed[next] = current
+						}
+
+						break
+					}
+				}
+			}
+		}
+
+		dirIter++
+
+		for next, current := range proposed {
+			_, ok := cannotMove[next]
+			if ok {
+				continue
+			}
+
+			g.delete(current)
+			g.set(next)
+		}
+
+		if rounds == limit {
+			var (
+				bounds = g.bounds()
+				height = bounds.maxY - bounds.minY + 1
+				width  = bounds.maxX - bounds.minX + 1
+				size   = height * width
+			)
+
+			return size - elves
+		}
+
+		if still == elves {
+			return rounds
+		}
+
+	}
+}
+
 func (g *grid) set(c utils.Coord) {
 	if _, ok := (*g)[c.Y]; !ok {
 		(*g)[c.Y] = make(map[int]struct{})
