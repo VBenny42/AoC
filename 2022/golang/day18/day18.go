bdc58422d2429c68e2784c1de957a5546d6f857b
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-01-23 12:33:00 -0400

    feat(2022): made day18 way faster, no goroutines needed

--- 2022/golang/day18/day18.go
+++ 2022/golang/day18/day18.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/VBenny42/AoC/2022/golang/utils"
 )
@@ -13,9 +12,17 @@ type cube struct {
 	x, y, z int
 }
 
+type droplet byte
+
+const (
+	air droplet = iota
+	lava
+	steam
+)
+
 type day18 struct {
 	cubes            []cube
-	grid             *[][][]bool
+	grid             *[][][]droplet
 	maxX, maxY, maxZ int
 }
 
@@ -28,118 +35,86 @@ var directions = []cube{
 	{x: 0, y: 0, z: -1},
 }
 
-func (d *day18) canReachEdge(c cube) bool {
-	queue := []cube{c}
-	seen := make(map[cube]bool)
+func (d *day18) floodFillWithSteam() {
+	var start cube
+
+	queue := []cube{start}
 
 	for len(queue) > 0 {
 		curr := queue[0]
 		queue = queue[1:]
 
-		isValid := curr.x >= 0 && curr.x <= d.maxX &&
-			curr.y >= 0 && curr.y <= d.maxY &&
-			curr.z >= 0 && curr.z <= d.maxZ
-
-		// Reached an edge
-		if !isValid {
-			return true
-		}
-
-		if seen[curr] || (*d.grid)[curr.z][curr.y][curr.x] {
+		if (*d.grid)[curr.z][curr.y][curr.x] != air {
 			continue
 		}
-		seen[curr] = true
+		(*d.grid)[curr.z][curr.y][curr.x] = steam
 
 		for _, dir := range directions {
-			next := cube{
+			neighbor := cube{
 				x: curr.x + dir.x,
 				y: curr.y + dir.y,
 				z: curr.z + dir.z,
 			}
 
-			queue = append(queue, next)
+			isValid := neighbor.x >= 0 && neighbor.x <= d.maxX+1 &&
+				neighbor.y >= 0 && neighbor.y <= d.maxY+1 &&
+				neighbor.z >= 0 && neighbor.z <= d.maxZ+1
+
+			if isValid && (*d.grid)[neighbor.z][neighbor.y][neighbor.x] == air {
+				queue = append(queue, neighbor)
+			}
 		}
 	}
-	return false
 }
 
 func (d *day18) Part1() int {
-	var (
-		coveredArea int
-		coveredChan = make(chan struct{})
-		wg          sync.WaitGroup
-	)
-	wg.Add(len(d.cubes))
+	var coveredArea int
 
 	for _, c := range d.cubes {
-		go func(c cube) {
-			defer wg.Done()
-
-			for _, dir := range directions {
-				neighbor := c
-				neighbor.x += dir.x
-				neighbor.y += dir.y
-				neighbor.z += dir.z
-
-				isValid := neighbor.x >= 0 && neighbor.x <= d.maxX &&
-					neighbor.y >= 0 && neighbor.y <= d.maxY &&
-					neighbor.z >= 0 && neighbor.z <= d.maxZ
-
-				if isValid && (*d.grid)[neighbor.z][neighbor.y][neighbor.x] {
-					coveredChan <- struct{}{}
-				}
+		for _, dir := range directions {
+			neighbor := cube{
+				x: c.x + dir.x,
+				y: c.y + dir.y,
+				z: c.z + dir.z,
 			}
-		}(c)
-	}
 
-	go func() {
-		wg.Wait()
-		close(coveredChan)
-	}()
+			isValid := neighbor.x >= 0 && neighbor.x <= d.maxX &&
+				neighbor.y >= 0 && neighbor.y <= d.maxY &&
+				neighbor.z >= 0 && neighbor.z <= d.maxZ
 
-	for range coveredChan {
-		coveredArea++
+			if isValid && (*d.grid)[neighbor.z][neighbor.y][neighbor.x] == lava {
+				coveredArea++
+			}
+		}
 	}
 
 	return 6*len(d.cubes) - coveredArea
 }
 
 func (d *day18) Part2() int {
-	var (
-		externalArea int
-		exteriorChan = make(chan struct{})
-		wg           sync.WaitGroup
-	)
-	wg.Add(len(d.cubes))
+	d.floodFillWithSteam()
+
+	var exteriorArea int
 
 	for _, c := range d.cubes {
-		go func(c cube) {
-			defer wg.Done()
-
-			for _, dir := range directions {
-				next := cube{
-					x: c.x + dir.x,
-					y: c.y + dir.y,
-					z: c.z + dir.z,
-				}
-
-				if d.canReachEdge(next) {
-					exteriorChan <- struct{}{}
-				}
+		for _, dir := range directions {
+			neighbor := cube{
+				x: c.x + dir.x,
+				y: c.y + dir.y,
+				z: c.z + dir.z,
 			}
-		}(c)
+
+			isValid := neighbor.x >= 0 && neighbor.x <= d.maxX+1 &&
+				neighbor.y >= 0 && neighbor.y <= d.maxY+1 &&
+				neighbor.z >= 0 && neighbor.z <= d.maxZ+1
+
+			if isValid && (*d.grid)[neighbor.z][neighbor.y][neighbor.x] == steam {
+				exteriorArea++
+			}
+		}
 	}
 
-	go func() {
-		wg.Wait()
-		close(exteriorChan)
-	}()
-
-	for range exteriorChan {
-		externalArea++
-	}
-
-	return externalArea
+	return exteriorArea
 }
 
 func Parse(filename string) *day18 {
@@ -149,26 +124,28 @@ func Parse(filename string) *day18 {
 
 	for i, line := range data {
 		split := strings.Split(line, ",")
+		// Adding padding for steam to reach cubes on the edges
 		cubes[i] = cube{
-			x: utils.Must(strconv.Atoi(split[0])),
-			y: utils.Must(strconv.Atoi(split[1])),
-			z: utils.Must(strconv.Atoi(split[2])),
+			x: utils.Must(strconv.Atoi(split[0])) + 1,
+			y: utils.Must(strconv.Atoi(split[1])) + 1,
+			z: utils.Must(strconv.Atoi(split[2])) + 1,
 		}
 		maxX = max(maxX, cubes[i].x)
 		maxY = max(maxY, cubes[i].y)
 		maxZ = max(maxZ, cubes[i].z)
 	}
 
-	grid := make([][][]bool, maxZ+1)
+	// Adding padding for steam to fill all around the cubes
+	grid := make([][][]droplet, maxZ+2)
 	for col := range grid {
-		grid[col] = make([][]bool, maxY+1)
+		grid[col] = make([][]droplet, maxY+2)
 		for row := range grid[col] {
-			grid[col][row] = make([]bool, maxX+1)
+			grid[col][row] = make([]droplet, maxX+2)
 		}
 	}
 
 	for _, c := range cubes {
-		grid[c.z][c.y][c.x] = true
+		grid[c.z][c.y][c.x] = lava
 	}
 
 	return &day18{
