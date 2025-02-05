acf6660b3c2e3962e7840eda5c73343748f14829
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-02-05 10:51:05 -0400

    feat(day24): part 2 done

--- 2023/golang/day24/day24.go
+++ 2023/golang/day24/day24.go
@@ -2,189 +2,219 @@ package day24
 
 import (
 	"fmt"
-	"math/big"
 	"strings"
 
 	"github.com/VBenny42/AoC/2023/golang/utils"
-	"gonum.org/v1/gonum/stat/combin"
 )
 
 type point struct {
-	x, y, z int64
+	x, y, z float64
 }
 
 type hailstone struct {
-	pos point
-	vel point
+	pos          point
+	vel          point
+	verticalPath bool
+	slope        float64
 }
 
 type day24 struct {
 	hailstones []hailstone
-	minBound   int64
-	maxBound   int64
+	minBound   float64
+	maxBound   float64
 }
 
-func (d *day24) Part1() (intersections int) {
-	combinations := combin.Combinations(len(d.hailstones), 2)
+func hailstonesParallel(h1, h2 hailstone) bool {
+	if h1.verticalPath && h2.verticalPath {
+		return true
+	}
+	return h1.slope == h2.slope
+}
 
-	for _, combo := range combinations {
-		h1 := d.hailstones[combo[0]]
-		h2 := d.hailstones[combo[1]]
+func intersectingCoords(h1, h2 hailstone) (x, y float64, ok bool) {
+	if hailstonesParallel(h1, h2) {
+		return
+	}
 
-		// Coefficients for the system of equations:
-		// vel.x1 * t1 - vel.x2 * t2 = pos.x2 - pos.x1
-		// vel.y1 * t1 - vel.y2 * t2 = pos.y2 - pos.y1
-		// Matrix form: [vel.x1, -vel.x2; vel.y1, -vel.y2] * [t1; t2] = [dx; dy]
-		var (
-			dx  = h2.pos.x - h1.pos.x
-			dy  = h2.pos.y - h1.pos.y
-			vx1 = h1.vel.x
-			vy1 = h1.vel.y
-			vx2 = h2.vel.x
-			vy2 = h2.vel.y
-		)
+	ok = true
+	x = (h1.slope*h1.pos.x - h2.slope*h2.pos.x + h2.pos.y - h1.pos.y) /
+		(h1.slope - h2.slope)
+	y = h1.slope*(x-h1.pos.x) + h1.pos.y
 
-		// Compute determinant
-		det := vx1*(-vy2) - (-vx2)*vy1
-		if det == 0 {
-			// Paths are parallel, check if they are the same line
-			// Check cross product: dx*vel.y1 - dy*vel.x1 == 0
-			if dx*vy1 != dy*vx1 {
-				continue
-			}
-			// Paths are the same line, check if there's any t1, t2 >=0 that brings both into the test area
-			if d.coincidingPathsIntersect(h1, h2) {
-				intersections++
-			}
-			continue
-		}
+	return
+}
 
-		// Solve for t1 and t2 using Cramer's rule with exact fractions
-		// t1 = (dx*(-vel.y2) - (-vel.x2)*dy) / det
-		// t2 = (vel.x1*dy - vel.y1*dx) / det
-		numeratorT1 := dx*(-vy2) - (-vx2)*dy
-		numeratorT2 := vx1*dy - vy1*dx
+func timeToIntersection(h hailstone, x float64) float64 {
+	return (x - h.pos.x) / h.vel.x
+}
 
-		// Check if t1 and t2 are non-negative
-		if (numeratorT1 > 0 && det < 0) || (numeratorT1 < 0 && det > 0) {
-			// t1 is negative
-			continue
-		}
-		if (numeratorT2 > 0 && det < 0) || (numeratorT2 < 0 && det > 0) {
-			// t2 is negative
-			continue
-		}
-
-		// Compute absolute values to check if numerator and denominator have the same sign
-		absNumT1 := numeratorT1
-		if absNumT1 < 0 {
-			absNumT1 = -absNumT1
-		}
-		absDet := det
-		if absDet < 0 {
-			absDet = -absDet
-		}
-
-		// Compute t1 and t2 as fractions to avoid precision loss
-		t1Num := big.NewInt(numeratorT1)
-		t1Den := big.NewInt(det)
-		t1Rat := new(big.Rat).SetFrac(t1Num, t1Den)
-
-		t2Num := big.NewInt(numeratorT2)
-		t2Den := big.NewInt(det)
-		t2Rat := new(big.Rat).SetFrac(t2Num, t2Den)
-
-		if t1Rat.Sign() < 0 || t2Rat.Sign() < 0 {
-			continue
-		}
-
-		// Calculate intersection point using h1's path at t1
-		xNum := big.NewInt((h1.pos.x))
-		xNum.Mul(xNum, t1Den)
-		xNum.Add(xNum, big.NewInt(h1.vel.x).Mul(big.NewInt(h1.vel.x), t1Num))
-		xDen := t1Den
-
-		yNum := big.NewInt(int64(h1.pos.y))
-		yNum.Mul(yNum, t1Den)
-		yNum.Add(yNum, big.NewInt(h1.vel.y).Mul(big.NewInt(h1.vel.y), t1Num))
-		yDen := t1Den
-
-		// Check if x and y are within the test area
-		if d.isWithinBounds(xNum, xDen) && d.isWithinBounds(yNum, yDen) {
-			intersections++
+func possibleVelocities(pos1, pos2 int, vel int) (velocities []int) {
+	for possible := -1000; possible < 1000; possible++ {
+		if possible != vel && (pos1-pos2)%(possible-vel) == 0 {
+			velocities = append(velocities, possible)
 		}
 	}
 
 	return
 }
 
-func (d *day24) isWithinBounds(num, den *big.Int) bool {
-	if den.Sign() == 0 {
-		return false
+func sliceIntersection(s1, s2 []int) (intersection []int) {
+	// for _, v1 := range s1 {
+	// 	for _, v2 := range s2 {
+	// 		if v1 == v2 {
+	// 			intersection = append(intersection, v1)
+	// 		}
+	// 	}
+	// }
+
+	m := make(map[int]struct{}, len(s1))
+	for _, v := range s2 {
+		m[v] = struct{}{}
 	}
-	minBound := big.NewRat(d.minBound, 1)
-	maxBound := big.NewRat(d.maxBound, 1)
 
-	val := new(big.Rat).SetFrac(num, den)
+	for _, v := range s1 {
+		if _, ok := m[v]; ok {
+			intersection = append(intersection, v)
+		}
+	}
 
-	return val.Cmp(minBound) >= 0 && val.Cmp(maxBound) <= 0
+	return
 }
 
-func (d *day24) coincidingPathsIntersect(h1, h2 hailstone) bool {
-	return d.pathEntersTestArea(h1) || d.pathEntersTestArea(h2)
+func (d *day24) Part1() (intersections int) {
+	for i, h1 := range d.hailstones {
+		for _, h2 := range d.hailstones[i+1:] {
+			x, y, ok := intersectingCoords(h1, h2)
+			if !ok {
+				continue
+			}
+
+			if timeToIntersection(h1, x) < 0 || timeToIntersection(h2, x) < 0 {
+				continue
+			}
+
+			if (x >= d.minBound && x <= d.maxBound) && (y >= d.minBound && y <= d.maxBound) {
+				intersections++
+			}
+		}
+	}
+
+	return
 }
 
-func (d *day24) pathEntersTestArea(h hailstone) bool {
-	// Check if the hailstone's path enters the test area at any time t >=0
-	enterX := false
-	if h.vel.x > 0 {
-		enterX = h.pos.x <= d.maxBound
-	} else if h.vel.x < 0 {
-		enterX = h.pos.x >= d.minBound
-	} else {
-		enterX = h.pos.x >= d.minBound && int64(h.pos.x) <= d.maxBound
+func (d *day24) Part2() (sum int) {
+	var possibleXVel, possibleYVel, possibleZVel []int
+
+	for i, h1 := range d.hailstones {
+		for _, h2 := range d.hailstones[i+1:] {
+			if h1.vel.x == h2.vel.x {
+				possibilities := possibleVelocities(int(h2.pos.x), int(h1.pos.x), int(h1.vel.x))
+				if len(possibleXVel) == 0 {
+					possibleXVel = possibilities
+				} else {
+					possibleXVel = sliceIntersection(possibleXVel, possibilities)
+				}
+			}
+			if h1.vel.y == h2.vel.y {
+				possibilities := possibleVelocities(int(h2.pos.y), int(h1.pos.y), int(h1.vel.y))
+				if len(possibleYVel) == 0 {
+					possibleYVel = possibilities
+				} else {
+					possibleYVel = sliceIntersection(possibleYVel, possibilities)
+				}
+			}
+			if h1.vel.z == h2.vel.z {
+				possibilities := possibleVelocities(int(h2.pos.z), int(h1.pos.z), int(h1.vel.z))
+				if len(possibleZVel) == 0 {
+					possibleZVel = possibilities
+				} else {
+					possibleZVel = sliceIntersection(possibleZVel, possibilities)
+				}
+			}
+		}
 	}
 
-	enterY := false
-	if h.vel.y > 0 {
-		enterY = h.pos.y <= d.maxBound
-	} else if h.vel.y < 0 {
-		enterY = h.pos.y >= d.minBound
-	} else {
-		enterY = h.pos.y >= d.minBound && int64(h.pos.y) <= d.maxBound
+	if len(possibleXVel) != 1 || len(possibleYVel) != 1 || len(possibleZVel) != 1 {
+		panic("could not narrow down to a single possible velocity")
 	}
 
-	return enterX && enterY
+	var (
+		rockVelX = float64(possibleXVel[0])
+		rockVelY = float64(possibleYVel[0])
+		rockVelZ = float64(possibleZVel[0])
+	)
+
+	// Find two hailstones with the same x velocity to ensure valid calculation
+	var h1, h2 hailstone
+	found := false
+	for i := range d.hailstones {
+		for j := i + 1; j < len(d.hailstones); j++ {
+			if d.hailstones[i].vel.x == d.hailstones[j].vel.x {
+				h1 = d.hailstones[i]
+				h2 = d.hailstones[j]
+				found = true
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+	if !found {
+		panic("no pair found with same x velocity")
+	}
+
+	var (
+		m1 = (h1.vel.y - rockVelY) / (h1.vel.x - rockVelX)
+		m2 = (h2.vel.y - rockVelY) / (h2.vel.x - rockVelX)
+		c1 = h1.pos.y - m1*h1.pos.x
+		c2 = h2.pos.y - m2*h2.pos.x
+	)
+
+	var (
+		rockX = (c2 - c1) / (m1 - m2)
+		rockY = m1*rockX + c1
+		time  = (rockX - h1.pos.x) / (h1.vel.x - rockVelX)
+		rockZ = h1.pos.z + (h1.vel.z-rockVelZ)*time
+	)
+
+	sum = int(rockX + rockY + rockZ)
+	return
 }
 
 func Parse(filename string, minBound, maxBound int) *day24 {
 	data := utils.ReadLines(filename)
 	hailstones := make([]hailstone, len(data))
 
-	fieldsFunc := func(r rune) bool {
-		return r == ' ' || r == '@' || r == ','
-	}
-
 	for i, line := range data {
-		fields := strings.FieldsFunc(line, fieldsFunc)
+		fields := strings.FieldsFunc(line, func(r rune) bool {
+			return r == ' ' || r == '@' || r == ','
+		})
 
-		hailstones[i].pos.x = int64(utils.Atoi(fields[0]))
-		hailstones[i].pos.y = int64(utils.Atoi(fields[1]))
-		hailstones[i].pos.z = int64(utils.Atoi(fields[2]))
-		hailstones[i].vel.x = int64(utils.Atoi(fields[3]))
-		hailstones[i].vel.y = int64(utils.Atoi(fields[4]))
-		hailstones[i].vel.z = int64(utils.Atoi(fields[5]))
+		hailstones[i].pos.x = float64(utils.Atoi(fields[0]))
+		hailstones[i].pos.y = float64(utils.Atoi(fields[1]))
+		hailstones[i].pos.z = float64(utils.Atoi(fields[2]))
+		hailstones[i].vel.x = float64(utils.Atoi(fields[3]))
+		hailstones[i].vel.y = float64(utils.Atoi(fields[4]))
+		hailstones[i].vel.z = float64(utils.Atoi(fields[5]))
 
+		hailstones[i].verticalPath = hailstones[i].vel.x == 0
+
+		if !hailstones[i].verticalPath {
+			hailstones[i].slope = hailstones[i].vel.y / hailstones[i].vel.x
+		}
 	}
 
 	return &day24{
 		hailstones: hailstones,
-		minBound:   int64(minBound),
-		maxBound:   int64(maxBound),
+		minBound:   float64(minBound),
+		maxBound:   float64(maxBound),
 	}
 }
 
 func Solve(filename string) {
 	day := Parse(filename, 200000000000000, 400000000000000)
+
 	fmt.Println("ANSWER1: number of intersections:", day.Part1())
+	fmt.Println("ANSWER2: sum of rock's coordinates to hit all hailstones:", day.Part2())
 }
