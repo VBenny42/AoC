21cc380ff27b95321f864801e5c55644c2f164e1
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-02-09 11:48:05 -0400

    feat(set): added set data structure, let's see if I use it

---
+++ 2021/golang/utils/set/set.go
@@ -0,0 +1,42 @@
+package set
+
+type Set[T comparable] map[T]struct{}
+
+// NewSet creates a new set with the given values.
+func NewSet[T comparable](values ...T) Set[T] {
+	s := make(Set[T])
+	for _, v := range values {
+		s[v] = struct{}{}
+	}
+	return s
+}
+
+// Add adds the given value to the set.
+func (s Set[T]) Add(value T) {
+	s[value] = struct{}{}
+}
+
+// Remove removes the given value from the set.
+func (s Set[T]) Remove(value T) {
+	delete(s, value)
+}
+
+// Contains returns true if the set contains the given value.
+func (s Set[T]) Contains(value T) bool {
+	_, ok := s[value]
+	return ok
+}
+
+// Len returns the number of elements in the set.
+func (s Set[T]) Len() int {
+	return len(s)
+}
+
+// Values returns a slice of all the values in the set.
+func (s Set[T]) Values() []T {
+	values := make([]T, 0, len(s))
+	for v := range s {
+		values = append(values, v)
+	}
+	return values
+}
