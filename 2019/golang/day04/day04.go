a5f93246ec03b9dae385c72ed9657798c2378b47
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-09-13 21:57:15 -0400

    feat(day04): initial brute force solution

---
+++ 2019/golang/day04/day04.go
@@ -0,0 +1,125 @@
+package day04
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/VBenny42/AoC/2019/golang/utils"
+)
+
+type day04 struct {
+	start, end int
+}
+
+type password [6]int
+
+func makePassword(n int) password {
+	var p password
+	for i := 5; i >= 0; i-- {
+		p[i] = n % 10
+		n /= 10
+	}
+	return p
+}
+
+func (p *password) isValid() (valid bool) {
+	for i := 1; i < 6; i++ {
+		if p[i] < p[i-1] {
+			return false
+		}
+		if p[i] == p[i-1] {
+			valid = true
+		}
+	}
+
+	return
+}
+
+func (p *password) isValid2() (valid bool) {
+	var count int
+	for i := 1; i < 6; i++ {
+		if p[i] == p[i-1] {
+			count++
+		} else {
+			if count == 1 {
+				valid = true
+			}
+			count = 0
+		}
+	}
+	if count == 1 {
+		valid = true
+	}
+
+	return
+}
+
+func (d *day04) Part1And2() (count1, count2 int) {
+	const numWorkers = 100
+	var (
+		wg       sync.WaitGroup
+		resultCh = make(chan [2]int, numWorkers)
+		jobCh    = make(chan int, numWorkers)
+	)
+
+	// Start workers
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			localCount1, localCount2 := 0, 0
+			for n := range jobCh {
+				p := makePassword(n)
+				if p.isValid() {
+					localCount1++
+					if p.isValid2() {
+						localCount2++
+					}
+				}
+			}
+			resultCh <- [2]int{localCount1, localCount2}
+		}()
+	}
+
+	// Send jobs
+	go func() {
+		for n := d.start; n <= d.end; n++ {
+			jobCh <- n
+		}
+		close(jobCh)
+	}()
+
+	// Close result channel after all workers finish
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+
+	// Collect results
+	for result := range resultCh {
+		count1 += result[0]
+		count2 += result[1]
+	}
+
+	return
+}
+
+func Parse(filename string) *day04 {
+	var (
+		day  day04
+		line = utils.ReadTrimmed(filename)
+	)
+
+	fmt.Sscanf(line, "%d-%d", &day.start, &day.end)
+
+	return &day
+}
+
+func Solve(filename string) {
+	day := Parse(filename)
+
+	count1, count2 := day.Part1And2()
+
+	fmt.Println("ANSWER1: number of valid passwords:", count1)
+	fmt.Println("ANSWER2: number of valid passwords with new double constraint:", count2)
+}
---
+++ 2019/golang/day04/day04_test.go
@@ -0,0 +1,30 @@
+package day04_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/VBenny42/AoC/2019/golang/day04"
+)
+
+func BenchmarkSolve(b *testing.B) {
+	// b.ReportAllocs()
+
+	oldStdout := os.Stdout
+	null, err := os.Open(os.DevNull)
+	if err != nil {
+		b.Fatal(err)
+	}
+	os.Stdout = null
+
+	defer func() {
+		os.Stdout = oldStdout
+		null.Close()
+	}()
+
+	b.ResetTimer()
+
+	for range b.N {
+		day04.Solve("inputs/day04/input.txt")
+	}
+}
