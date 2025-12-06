9132c8808d1379e03f01cdf5c725ff633c64eb1a
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2025-12-06 14:22:19 -0400

    feat(day6): part 2 done

--- 2025/rust/src/bin/06.rs
+++ 2025/rust/src/bin/06.rs
@@ -2,6 +2,7 @@ advent_of_code::solution!(6);
 
 use std::str::FromStr;
 
+#[derive(Clone, Debug)]
 struct Problem {
     operator: char,
     numbers: Vec<u64>,
@@ -83,16 +84,88 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(day.problems.iter().map(|p| p.evaluate()).sum())
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    let day = match Day06::from_str(input) {
-        Ok(day) => day,
-        Err(error) => {
-            eprintln!("Error parsing input: {:?}", error);
-            return None;
+fn transpose(input: &str) -> Vec<String> {
+    let lines = input.lines().collect::<Vec<&str>>();
+    let longest_line_length = lines.iter().map(|line| line.len()).max().unwrap_or(0);
+
+    let padded_lines: Vec<String> = lines
+        .iter()
+        .map(|line| {
+            if line.len() < longest_line_length {
+                format!("{}{}", line, " ".repeat(longest_line_length - line.len()))
+            } else {
+                line.to_string()
+            }
+        })
+        .collect();
+
+    let mut transposed = vec![String::new(); longest_line_length];
+    for line in padded_lines {
+        for (i, ch) in line.chars().enumerate() {
+            transposed[i].push(ch);
         }
+    }
+    transposed
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let transposed_lines = transpose(input);
+
+    let mut problems = Vec::<Problem>::new();
+    let mut on_new_problem = true;
+    let mut current_problem = Problem {
+        operator: '+',
+        numbers: Vec::new(),
     };
 
-    None
+    for line in transposed_lines {
+        let trimmed = line.trim();
+        let len = trimmed.len();
+
+        match on_new_problem {
+            true => {
+                let operator = trimmed.chars().last().unwrap();
+                if operator != '+' && operator != '*' {
+                    eprintln!("Error parsing input: Invalid operator {}", operator);
+                    return None;
+                }
+
+                let first_number = match &trimmed[..len - 1].trim().parse::<u64>() {
+                    Ok(num) => *num,
+                    Err(e) => {
+                        eprintln!("Error parsing input: Invalid number {}", e);
+                        return None;
+                    }
+                };
+
+                current_problem = Problem {
+                    operator,
+                    numbers: vec![first_number],
+                };
+                on_new_problem = false;
+            }
+            false => match len {
+                0 => {
+                    problems.push(current_problem.clone());
+                    on_new_problem = true;
+                }
+                _ => match trimmed.parse::<u64>() {
+                    Ok(num) => current_problem.numbers.push(num),
+                    Err(e) => {
+                        eprintln!("Error parsing input: Invalid number {}", e);
+                        return None;
+                    }
+                },
+            },
+        }
+    }
+
+    // Push last problem if not already pushed
+    if !on_new_problem {
+        problems.push(current_problem);
+    }
+
+    Some(problems.iter().map(|p| p.evaluate()).sum())
 }
 
 #[cfg(test)]
@@ -108,6 +181,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
